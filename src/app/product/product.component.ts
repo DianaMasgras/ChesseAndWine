50621a4ffabff9e571b3b1934941478698ccc1ea
DianaMasgras <dianamadalina30@gmail.com>
2021-06-20 01:19:47 +0300

    create product service

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -3,11 +3,10 @@ import { Component } from '@angular/core';
 export interface Product {
   title: string;
   describe: string;
-  price:number;
-  available:boolean;
+  price: number;
+  available: boolean;
   image: string;
 }
-
 @Component({
   selector:'product',
   templateUrl: './product.component.html',
